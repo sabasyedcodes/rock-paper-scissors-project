d67c45cab2e2ec5a1dfe2f66aa2d569148b85ed1
sabasyedcodes <sabaa9018@gmail.com>
2023-10-16 10:46:02 +0530

    play with keyboard feature added

--- script.js
+++ script.js
@@ -20,6 +20,15 @@ let score = JSON.parse(localStorage.getItem('score')) ||{
           isAutoPlaying = false;
         }
       }
+      document.body.addEventListener('keydown',(event)=>{
+        if(event.key ==='r'){
+          playGame('rock');
+        }else if (event.key === 'p'){
+          playGame('paper');
+        }else if(event.key === 's'){
+          playGame('scissors');
+        }
+      });
       function playGame(playerMove){
         const computerMove = pickComputerMove();
 
